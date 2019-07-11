9349b9eda0ea26db44c655e183cc8adb3e7c1365
luminousAvocado <jamesckang@gmail.com>
2019-07-10 17:19:36 -0700

    Some initial creations of Dentist properties

--- WebApiProj/Models/Dentist.cs
+++ WebApiProj/Models/Dentist.cs
@@ -7,5 +7,15 @@ namespace WebApiProj.Models
 {
     public class Dentist
     {
+        public int DentistId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        //public IList<Patient> Patients { get; set; }
     }
 }
