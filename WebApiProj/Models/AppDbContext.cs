223a44fb2ba6f168910116ad0a24d54d93cc5307
luminousAvocado <jamesckang@gmail.com>
2019-07-10 17:23:34 -0700

    Added Dentist to dbContext

--- WebApiProj/Models/AppDbContext.cs
+++ WebApiProj/Models/AppDbContext.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApiProj.Models
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
 
+        public DbSet<Dentist> Dentists { get; set; }
     }
 }
