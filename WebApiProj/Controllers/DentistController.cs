a8d218e90f8fdb7c59f189f9a615eef723090959
luminousAvocado <jamesckang@gmail.com>
2019-07-10 17:42:49 -0700

    Initial creation of Dentist controller

---
+++ WebApiProj/Controllers/DentistController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiProj.Models;
+
+namespace WebApiProj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DentistController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DentistController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
