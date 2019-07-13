0b7a484be0af661c9dae448a89a45ebea585a81d
luminousAvocado <jamesckang@gmail.com>
2019-07-13 14:41:43 -0700

    DUnno why this change didnt get added to last add

--- WebApiProj/Startup.cs
+++ WebApiProj/Startup.cs
@@ -63,6 +63,7 @@ namespace WebApiProj
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
+                app.UseExceptionHandler();
             }
 
             app.UseAuthentication();
